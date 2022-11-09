639961c59f4200a9e12a3e0d09b9eb2ba27a0339
SSollestre <sean.sollestre@gmail.com>
2022-11-08 17:55:12 -0800

    Start working on data

---
+++ meals_data/nutrition.js
@@ -0,0 +1,26 @@
+
+//------------------------------------------------------
+// Get data from a CSV file with ".fetch()"
+// Since this file is local, you must use "live serve"
+//------------------------------------------------------
+async function getCSVdata() {
+    const response = await fetch('./foods.csv'); //send get request
+    const data = await response.text();      //get file response
+    const list = data.split('\n').slice(1);  //get line
+    list.length = 10
+    list.forEach(row => {
+        const columns = row.split(','); //get token 
+        const id = columns[0];     //country name
+        const name = columns[1];        //gold medals
+        const calories = columns[2];
+        console.log(name)
+        //db.collection("countries").add({   //write to firestore
+        //    name: country,
+        //    details: details
+        //})
+
+    })
+}
+
+getCSVdata()
+
