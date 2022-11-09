9b9469d1c8dfc7c84a7672c4375a1c99f786f7a9
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2022-11-09 01:03:22 -0800

    Add get data code to goals.js and goals_02a

---
+++ goals_data/goals.js
@@ -0,0 +1,44 @@
+//add windows api
+//goals 1 displays graph from goals_02a and goals_02b
+
+function Financial() {
+    console.log("check")
+    let WeeklyBudget = document.getElementById("weeklyBudget").value;
+    let SaveAmount = document.getElementById("saveAmount").value;
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    var userEmail = userDoc.data().email;
+                    db.collection("Financial").add({
+                        userID: userID,
+                        weeklybudget: WeeklyBudget,
+                        saveamount: SaveAmount.
+                            serverTimestamp()
+                    }).then(() => {
+                        window.location.href = "thanks.html"; //new line added
+                    })
+                })
+
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+
+function saveUserInfo() {
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    WeeklyBudget = document.getElementById('weeklyBudget').value;     //get the value of the field with id="schoolInput"
+    SaveAmount = document.getElementById('saveAmount').value;       //get the value of the field with id="cityInput"
+    currentUser.update({
+        weeklybudget: WeeklyBudget,
+        saveamount: SaveAmount
+    })
+        .then(() => {
+            console.log("Document successfully updated!");
+        })
+}
