5fac5c057006ed67e5801fa1318e1e19d67b2e81
Robert Oh <soh67@my.bcit.ca>
2022-11-18 12:43:46 -0800

    update groceries_02a.html and groceries_02a.js

--- scripts/groceries_02a.js
+++ scripts/groceries_02a.js
@@ -11,3 +11,36 @@ function saveUserInfo() {
     }
     )
 }
+
+
+function clear_item() {
+    firebase.auth().onAuthStateChanged(user => {
+        var currentUser = db.collection("users").doc(user.uid)
+        food_amount = document.getElementById('data_input').value;     //get the value of the field with id="schoolInput"
+        currentUser.update({
+            food_amount: 0
+        })
+            .then(() => {
+                console.log("Document successfully updated!");
+            })
+    }
+    )
+}
+
+
+function populate_span() {
+    firebase.auth().onAuthStateChanged(user => {
+        var currentUser = db.collection("users").doc(user.uid)
+        currentUser.get().then(user_doc => {
+            var food_amount = user_doc.data().food_amount
+            document.getElementById("amount").innerHTML = food_amount
+            console.log(user_doc)
+        })
+
+
+    }
+    )
+}
+
+
+populate_span()
