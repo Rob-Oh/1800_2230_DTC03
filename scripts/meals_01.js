a490ddab2fdc4495ca6e3dda2b4f76a6c37d5aae
SSollestre <sean.sollestre@gmail.com>
2022-11-21 19:43:38 -0800

    Implement recommended daily calories

--- scripts/meals_01.js
+++ scripts/meals_01.js
@@ -42,16 +42,37 @@ function delete_log() {
 
 function graph_overall() {
     var calories = 0
-    var barColors = ["red"];
+    var goal_calories = 0
+    var barColors = ["red", "green"];
     Chart.defaults.font.size = 15;
 
 
     function calculate() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                var currentUser = db.collection("users").doc(user.uid)
+                currentUser.get().then(user_doc => {
+                    var user_weight = parseInt(user_doc.data().weight);
+                    var user_age = parseInt(user_doc.data().age);
+                    var user_height = parseInt(user_doc.data().height);
+                    var user_sex = user_doc.data().sex
+                    // console.log(typeof user_weight)
+                    // console.log(typeof user_age)
+                    // console.log(typeof user_height)
+                    // console.log(typeof user_sex)
+                    if (user_sex == "male") {
+                        goal_calories = 66.5 + (13.75 * user_weight) + (5.003 * user_height) - (6.75 * user_age)
+                    }
+                    if (user_sex == "female") {
+                        goal_calories = 655.1 + (9.563 * parseInt(user_weight)) + (1.850 * parseInt(user_height)) - (4.676 * parseInt(user_age))
+                    }
+                })
+            }
+        })
         db.collection("logs").get()
             .then(collection => {
                 collection.forEach((doc) => {
                     calories = calories + parseInt(doc.data().calories)
-                    console.log(calories)
                 })
             })
     }
@@ -59,21 +80,24 @@ function graph_overall() {
 
     function chart_make() {
         calculate()
-        var yValues = [calories];
-        var xValues = ["Calories"];
-        var yValues = [calories]
+        console.log("Goal calories:", goal_calories)
+        var yValues = [calories, goal_calories];
+        var xValues = ["Calories", "Goal"];
         new Chart("myChart", {
             type: "bar",
             data: {
                 labels: xValues,
                 datasets: [{
-                    label: "Today's Calories:",
+                    label: "Calorie Tracker",
                     barPercentage: 0.2,
                     backgroundColor: barColors,
                     data: yValues
                 }]
             },
             options: {
+                label: {
+                    display: false
+                },
                 scales: {
                     y: { beginAtZero: true }
                 }
@@ -81,7 +105,7 @@ function graph_overall() {
         });
     }
     calculate()
-    setTimeout(chart_make, 600);
+    setTimeout(chart_make, 700);
 }
 
 
