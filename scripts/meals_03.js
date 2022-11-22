d253c7c3289a2a63da08316a13e4214cac15ba5d
SSollestre <sean.sollestre@gmail.com>
2022-11-21 20:40:56 -0800

    Implement calorie multiplier

--- scripts/meals_03.js
+++ scripts/meals_03.js
@@ -12,12 +12,19 @@ function get_food_details() {
 
 function add_item() {
     var food_id = localStorage.getItem("food_id");
-    var food_name = localStorage.getItem("food_name");
-    var calories = localStorage.getItem("calories");
+    var calories = parseInt(localStorage.getItem("calories"));
+    var food_amount = parseInt(document.getElementById('food_amount').value);
+    var multiplied_calories = calories * Math.floor(food_amount / 100);
+    var food_name = localStorage.getItem("food_name") + " " + food_amount + "g";
+    console.log(food_name)
+    console.log(multiplied_calories)
+    console.log(typeof calories)
+    console.log(typeof food_amount)
+
     db.collection("logs").add({
         name: food_name,
         id: food_id,
-        calories: calories,
+        calories: multiplied_calories,
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
     }).then(() => {
         window.location.href = "./meals_01.html";
